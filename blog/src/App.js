87d526314acf491214e2edb74973d799772817b1
Eric DS <eric.d.s.info@gmail.com>
2021-11-09 23:26:35 +0000

    finished navbar step 2.8 completed

--- blog/src/App.js
+++ blog/src/App.js
@@ -10,11 +10,12 @@ import Home from "./pages/Home"
 import About from "./pages/About"
 import ArticlesList from "./pages/ArticlesList"
 import Article from "./pages/Article"
-
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <Router>
+      <Navbar/>
       <div className="max-w-screen-md mx-auto pt-20">
         <Routes>
           <Route exact path="/" element={ <Home/> }/>
