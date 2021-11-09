40023bd14c33cfa421364eba51039626a559928a
Eric DS <eric.d.s.info@gmail.com>
2021-11-09 18:31:43 +0000

    developing step 2.5

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,13 +1,13 @@
 import Home from "./pages/Home"
-import {BrowserRouter as Routes, Route} from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 function App() {
   return (
-    <Routes>
-      <div className="max-w-screen-md mx-auto pt-20">
-        <Route exact path="/" component={Home} />        
-      </div>
-    </Routes>
+    <Router>
+          <Route exact path="/">
+            <Home />
+          </Route>
+    </Router>
   );
 }
 
