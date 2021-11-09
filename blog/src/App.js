622d05d3b3b866c81bbd7c314f2a044b6bf35962
Eric DS <eric.d.s.info@gmail.com>
2021-11-09 23:09:37 +0000

    Now react-router-dom is working

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,12 +1,18 @@
 import Home from "./pages/Home"
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes, 
+  Route,
+  } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-          <Route exact path="/">
-            <Home />
-          </Route>
+      <div className="max-w-screen-md mx-auto pt-20">
+        <Routes>
+          <Route exact path="/" element={ <Home/> }/>
+        </Routes>        
+      </div>
     </Router>
   );
 }
--- blog/src/pages/Home.js
+++ blog/src/pages/Home.js
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div className="mb-20">
       <h1 className="sm:text-4xl text-2xl font-bold mt-6 mb-2 text-gray-900">
-        Hello welocme to my blog!
+        Hello welcome to my blog!
       </h1>
       <p className="mx-auto leading-relaxed text-base mb-4"> 
         lorem ipsum Tempor magna deserunt ullamco aute duis proident 
