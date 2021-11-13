a225aaa60a870b3e6e16017a744e2caa7100bebd
Eric DS <eric.d.s.info@gmail.com>
2021-11-13 16:53:48 +0000

    backend body parser and properties use

--- blog-backend/src/server.js
+++ blog-backend/src/server.js
@@ -1,7 +1,10 @@
 const express = require("express")
+const bodyParser = require("body-parser")
 
 const app = express();
+app.use(bodyParser.json());
 
 app.get("/hello", (req, res) => res.send("hello"));
+app.post("/hello", (req, res) => res.send(`Hello ${req.body.name}`));
 
 app.listen(8000, () => console.log("listening on port 8000"));
