ceb294843e57bbefcb613dceb0af38d16e105d31
Eric DS <eric.d.s.info@gmail.com>
2021-11-14 18:52:20 +0000

    failure on comment update, database works

--- blog/src/pages/Article.js
+++ blog/src/pages/Article.js
@@ -1,6 +1,3 @@
-import "whatwg-fetch" // In order to make fetch work in 
-                      // IE we have to install this package
-
 import React, {useState, useEffect} from 'react'
 import { useParams } from 'react-router-dom';
 
@@ -8,9 +5,10 @@ import articlesContent from './article-content';
 import Articles from '../components/Articles';
 import NoMatch from './NoMatch';
 import CommentsList from "../components/CommentsList";
+import AddCommentForm  from "../components/AddCommentForm";
 
 const Article = () => {
-  const { name } = useParams();
+  const {name}  = useParams();
   const article = articlesContent.find((article)=> article.name === name);
 
   const [articleInfo, setArticleInfo] = useState({ comments: []});
@@ -33,11 +31,17 @@ const Article = () => {
         { article.title }
       </h1>
       {article.content.map((paragraph, index) =>(
-        <p className="mx-auto leading-relaxed text-base mb-4">{paragraph}</p>
+        <p className="mx-auto leading-relaxed text-base mb-4" key={index}>
+          {paragraph}
+        </p>
       ))}
-        <CommentsList comments={ !articleInfo ? null : articleInfo.comments} />
+
+      <CommentsList comments={ !articleInfo ? null : articleInfo.comments} />
+      <AddCommentForm articleName={name} setArticleInfo={setArticleInfo} />
       
-      <h1 className="sm:text-2x text-xl font-bold mt-12 mb-4 text-gray-900">Other articles</h1>
+      <h1 className="sm:text-2x text-xl font-bold mt-12 mb-4 text-gray-900">
+        Other articles
+      </h1>
       <div className="flex flex-wrap -m-4">
         <Articles articles={articlesContent.filter(article => article.name !== name )}/>
       </div>  
