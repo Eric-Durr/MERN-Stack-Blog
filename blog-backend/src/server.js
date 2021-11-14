2ab74540fcfcfb1d2d2e489f66c1d125374ad400
Eric DS <eric.d.s.info@gmail.com>
2021-11-14 13:02:42 +0000

    backend using mongo for sotrage

--- blog-backend/src/server.js
+++ blog-backend/src/server.js
@@ -6,27 +6,48 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/api/articles/:name', async (req,res)=> {
+const withDB = async(operations) => {
   try {
-    const articleName = req.params.name;
     const client = await MongoClient.connect('mongodb://localhost:27017', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
     const db = client.db("mern-blog");
-    const articleInfo = await db.collection('articles').findOne({name: articleName})
-    res.status(200).json(articleInfo)
+    await operations(db);
     client.close();
   } catch(err) {
     res.status(500).json({message: "Can't connecto to database", err});
   }
+}
+
+app.get('/api/articles/:name', async (req,res)=> {
+    withDB( async (db) => {
+      const articleName = req.params.name;
+      const articleInfo = await db
+        .collection('articles')
+        .findOne({name: articleName});
+      res.status(200).json(articleInfo);
+    }, res);
 });
 
 app.post('/api/articles/:name/add-comments', (req, res) => {
-  const { username, text } = req.body;
   const articleName = req.params.name;
+  const { username, text } = req.body;
 
-  articlesInfo[articleName].comments.push({username, text});
-  res.status(200).send(articlesInfo[articleName]);
+  withDB( async (db) => {
+    const articleInfo = await db.collection('articles').findOne({name: articleName})
+    await db.collection('articles').updateOne(
+      {name: articleName}, 
+      {
+        '$set' : {
+          comments: articleInfo.comments.concat({username, text}),
+        },
+      }
+    );
+    const updateArticleInfo = await db
+    .collection('articles')
+    .findOne({name: articleName});
+    res.status(200).json(updateArticleInfo);
+  }, res);
 });
 app.listen(8000, () => console.log("listening on port 8000"));
