d90e03befea7def092306f4531aa8624dbbc8aea
Eric DS <eric.d.s.info@gmail.com>
2021-11-14 11:44:45 +0000

    finished extra basic API

--- blog-backend/src/server.js
+++ blog-backend/src/server.js
@@ -1,11 +1,26 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 
+const articlesInfo = {
+  "learn-react": {
+    comments: [],
+  },
+  "learn-node":{
+    comments: [],
+  },
+  "my-thougths-on-learning-react":{
+    comments: [],
+   },
+}
+
 const app = express();
 app.use(bodyParser.json());
 
-app.get("/hello", (req, res) => res.send("hello"));
-app.post("/hello", (req, res) => res.send(`Hello ${req.body.name}`));
-app.get("/hello/:name", (req, res) => res.send(`Hello ${req.params.name}!` ));
+app.post('/api/articles/:name/add-comments', (req, res) => {
+  const { username, text } = req.body;
+  const articleName = req.params.name;
 
+  articlesInfo[articleName].comments.push({username, text});
+  res.status(200).send(articlesInfo[articleName]);
+});
 app.listen(8000, () => console.log("listening on port 8000"));
